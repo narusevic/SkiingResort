25aab28452fa6a3d061fa4792cddd2a7bd068752
Lukas Naruševičius <narusevic@gmail.com>
2017-09-05 15:19:03 +0300

    Add project files.

---
+++ SkiingResort/Models/Weather.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkiingResort.Models
+{
+    public class Weather
+    {
+        public int Temperature { get; set; }
+        public float Humidity { get; set; }
+        public float Cloudiness { get; set; }
+    }
+}
