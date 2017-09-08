d6cf5a2ad279179e56e508af50336be0f9211e2d
michaalx <michalito.stankiewicz@gmail.com>
2017-09-08 12:43:39 +0200

    Pridėjau Track.

--- SkiingResort/Models/Track.cs
+++ SkiingResort/Models/Track.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
- 
 namespace SkiingResort.Models
 {
     public class Track
     {
         public string Name { get; set; }
         public int Level { get; set; }
-        public float Capacity { get; set; }
+        public int Capacity { get; set; }
+        public List<Customer> TrackCustomers { get; set; }
     }
 }
